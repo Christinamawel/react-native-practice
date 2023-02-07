8b86a051d6c1595cd49870de3c61a811c013f88f
Christinamawel <christinamawel@gmail.com>
2023-02-07 13:33:05 -0800

    add TouchableOpacity

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -13,6 +13,13 @@ export default function App() {
     { name: 'Bowser', id: '7'},
   ]);
 
+  const pressHandler = (id: string) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id)
+    })
+  }
+
   return (
     <View style={styles.container}>
 
@@ -21,17 +28,11 @@ export default function App() {
       keyExtractor={(item) => item.id}
       data={people}
       renderItem={({ item })=> (
-        <Text style={styles.item}>{item.name}</Text>
+        <TouchableOpacity onPress={() => pressHandler(item.id)}>
+          <Text style={styles.item}>{item.name}</Text>
+        </TouchableOpacity>
       )}
     />
-
-    {/* <ScrollView>
-      {people.map(item => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-      ))}
-    </ScrollView> */}
 
       <StatusBar style="auto" />
     </View>
