57676a240e3e707d9af59b4f81f72eadf81256de
Christinamawel <christinamawel@gmail.com>
2023-02-07 14:49:22 -0800

    make to do app

--- App.tsx
+++ App.tsx
@@ -1,57 +1,72 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/Header.js';
+import TodoItem from './components/TodoItem.js';
+import AddTodo from './components/addTodo.js';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'Christy', id: '1'},
-    { name: 'Yoshi', id: '2'},
-    { name: 'Mario', id: '3'},
-    { name: 'Luigi', id: '4'},
-    { name: 'Peach', id: '5'},
-    { name: 'Toad', id: '6'},
-    { name: 'Bowser', id: '7'},
-  ]);
+  const [todos, setTodos] = useState([
+    { text: 'clean kitchen', key: '1'},
+    { text: 'create an app', key: '2'},
+    { text: 'apply for jobs', key: '3'},
+    { text: 'exercise', key: '4'}
+  ])
 
-  const pressHandler = (id: string) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id)
+  const pressHandler = (key: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo: {text: string, key: string; }) => todo.key != key);
     })
   }
 
+  const submitHandler = (text: string) => {
+
+    if(text.length > 2) {
+      setTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          { text: text, key: Math.random().toString() }
+        ]
+      })
+    } else {
+      Alert.alert('OOPS!', 'to dos must be over 2 chars long.', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ])
+    }
+  }
+
   return (
-    <View style={styles.container}>
-
-    <FlatList
-      numColumns={2}
-      keyExtractor={(item) => item.id}
-      data={people}
-      renderItem={({ item })=> (
-        <TouchableOpacity onPress={() => pressHandler(item.id)}>
-          <Text style={styles.item}>{item.name}</Text>
-        </TouchableOpacity>
-      )}
-    />
-
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList 
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+        <StatusBar backgroundColor='#bd5d3a' style="light" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
+    backgroundColor: '#fff'
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 5,
-    width: 155
+  content : {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
   }
 });
